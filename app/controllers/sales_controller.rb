c32311fd16b03bd628af90d1d05796c55dc9637f
Icelle <icelleku@gmail.com>
2013-12-10 22:18:50 -0500

    Print sales

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,5 +1,6 @@
 class SalesController < ApplicationController
   def index
     # make your sales objects available to the correlating view here
+    @sales = ((Employee.load(Rails.root.to_s+"/app/assets/csv/employees.csv", Rails.root.to_s+"/app/assets/csv/sales_data.csv").find_all {|e| !e.sales.nil?}).map{|employee| employee.sales}).flatten
   end
 end
