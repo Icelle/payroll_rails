ef2e2d9ee7ecb2b67951012fc7cba35c444f2d80
Icelle <icelleku@gmail.com>
2013-12-10 22:18:40 -0500

    Print employees

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,7 @@
 class EmployeesController < ApplicationController
   def index
+    @employees = Employee.load(Rails.root.to_s+"/app/assets/csv/employees.csv", Rails.root.to_s+"/app/assets/csv/sales_data.csv")
+    # byebug
     # make your employee objects available to the correlating view here
   end
 end
